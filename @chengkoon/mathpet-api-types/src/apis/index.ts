e2949325452aaf4efbe08109e5123105844a4162
chengkoon <chengkoon@gmail.com>
2025-09-08 00:34:17 +0800

    add mathpet-api-types (for LLM consumption)

---
+++ @chengkoon/mathpet-api-types/src/apis/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ActivitiesApi';
+export * from './ActuatorApi';
+export * from './AnnotationsApi';
+export * from './AuthApi';
+export * from './ClaudeApi';
+export * from './ExamPaperImportControllerApi';
+export * from './FileControllerApi';
+export * from './NotificationsApi';
+export * from './PacksApi';
+export * from './PracticeApi';
+export * from './QuestionsApi';
+export * from './SyllabusImportControllerApi';
+export * from './TopicsApi';
